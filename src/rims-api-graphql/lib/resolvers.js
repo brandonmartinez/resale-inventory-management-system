098c7fc596ce8d9ece80178b53e854ab2dbd7d36
Brandon Martinez <me@brandonmartinez.com>
2021-09-21 00:25:05 -0400

    getting basic API setup

--- src/rims-api-graphql/lib/resolvers.js
+++ src/rims-api-graphql/lib/resolvers.js
@@ -5,19 +5,9 @@ const resolvers = {
 		}
 	},
 	Mutation: {
-		createInventoryItem(_, { inventoryItem }) {
-			// Look here: https://www.npmjs.com/package/apollo-datasource-cosmosdb
-			// And here: https://github.com/Azure-Samples/js-e2e-graphql-cosmosdb-static-web-app/blob/main/api/graphql/data/cosmos/GameDataSource.ts
-			// Read this: https://xuorig.medium.com/graphql-mutation-design-anemic-mutations-dd107ba70496
-			// and this: https://www.apollographql.com/blog/graphql/basics/designing-graphql-mutations/
-			// and this: https://labs.getninjas.com.br/sharing-data-in-a-microservices-architecture-using-graphql-97db59357602
-			// should the partition key be changed: https://docs.microsoft.com/en-us/azure/cosmos-db/partitioning-overview
-
-			console.log(inventoryItem);
-
-			return {
-				...inventoryItem
-			};
+		async createInventoryItem(_, { inventoryItem }, { dataSources }) {
+			const result = await dataSources.inventoryItems.createOne(inventoryItem);
+            return result.resource;
 		}
 	}
 };
