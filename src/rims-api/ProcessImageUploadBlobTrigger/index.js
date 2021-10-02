68a265d18ef1af1c1347d8b9a1d8d6748cbff35c
Brandon Martinez <me@brandonmartinez.com>
2021-10-02 00:02:36 -0400

    Moved to generic API folder

---
+++ src/rims-api/ProcessImageUploadBlobTrigger/index.js
@@ -0,0 +1,9 @@
+module.exports = async function (context, myBlob) {
+	context.log(
+		'JavaScript blob trigger function processed blob \n Blob:',
+		context.bindingData.blobTrigger,
+		'\n Blob Size:',
+		myBlob.length,
+		'Bytes'
+	);
+};
