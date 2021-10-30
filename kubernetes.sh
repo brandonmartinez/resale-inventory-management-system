65ae7944aff71b9b417285aa4329be2c80bd9799
Brandon Martinez <me@brandonmartinez.com>
2021-10-30 01:02:46 -0400

    Ingresses now working

--- kubernetes.sh
+++ kubernetes.sh
@@ -12,7 +12,7 @@ COSMOSDBCONNECTIONSTRING=$(echo $COSMOSDBCONNECTIONSTRING | base64 -)
 ASSETSSTORAGECONNECTIONSTRING=$(echo $ASSETSSTORAGECONNECTIONSTRING | base64 -)
 
 echo "Applying Nginx Ingress Controller"
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.4/deploy/static/provider/baremetal/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.4/deploy/static/provider/cloud/deploy.yaml
 
 echo "Applying RIMS Service Stack"
 envsubst < kubernetes.yml | kubectl apply -f -
